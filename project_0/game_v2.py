bf2f63d2adb5b6a9c1bc071a0fc043c86b23272b
Karim <k.kerimow@yandex.ru>
2025-02-07 21:31:49 +0300

    Обновления в game_v2.py

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -28,6 +28,33 @@ def random_predict(number: int=1) -> int:
 # print(f'Количество попыток: {random_predict()}')
 
 
+def game_core_v2(number: int = 1) -> int:
+    """Сначала устанавливаем любое random число, а потом уменьшаем
+    или увеличиваем его в зависимости от того, больше оно или меньше нужного.
+       Функция принимает загаданное число и возвращает число попыток
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+    predict = np.random.randint(1, 101)
+
+    while number != predict:
+        count += 1
+        if number > predict:
+            predict += 1
+        elif number < predict:
+            predict -= 1
+
+    return count
+
+
+# print(game_core_v2())
+
+
 def score_game(random_predict) -> int:
     """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
 
@@ -52,5 +79,10 @@ def score_game(random_predict) -> int:
 
 
 if __name__ == '__main__':
-    # RUN
-    score_game(random_predict) 
+
+    #Run benchmarking to score effectiveness of all algorithms
+    print('Run benchmarking for random_predict: ', end='')
+    score_game(random_predict)
+
+    print('Run benchmarking for game_core_v2: ', end='')
+    score_game(game_core_v2)
