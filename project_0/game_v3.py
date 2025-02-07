04a714dc712068bc8928b3f9aa66bce71c6504f9
Karim <k.kerimow@yandex.ru>
2025-02-07 22:07:59 +0300

    Обновлён game_v3.py

---
+++ project_0/game_v3.py
@@ -0,0 +1,20 @@
+from game_v2 import score_game
+
+
+def game_core_v3(number: int = 1) -> int:
+    """
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    # Ваш код начинается здесь
+
+    # Ваш код заканчивается здесь
+
+    return count
+
+
+print('Run benchmarking for game_core_v3: ', end='')
+score_game(game_core_v3) 
